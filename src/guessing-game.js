e4edc9cc58486bcd70d9f44ed3c8bf23c72c2c26
IrinaAgeenko <ira.ageenkoo@gmail.com>
2017-09-20 22:52:49 +0300

    almost working game exept 9 cases

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	this.min = min; 
+    	this.max = max;
     }
 
     guess() {
-
+    	return Math.floor((this.max + this.min)/2);
     }
 
     lower() {
-
+    	this.max = Math.floor((this.max + this.min)/2)+1;
     }
 
     greater() {
-
+    	this.min = Math.floor((this.max + this.min)/2);
     }
 }
 

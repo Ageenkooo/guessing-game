03cc2937cfb359883d1ab9693466fecc5a0d79b6
IrinaAgeenko <ira.ageenkoo@gmail.com>
2017-09-24 13:29:14 +0300

    working version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-    	this.min = min; 
-    	this.max = max;
+        this.arr = [];
+        for (var i = 0; i < max-min +1; i++) {
+            this.arr[i] = min + i;
+        }
+        this.left = 0; 
+        this.right = this.arr.length ;
     }
 
     guess() {
-    	return Math.floor((this.max + this.min)/2);
+        if(this.left < this.right)
+            return this.arr[Math.floor((this.left+this.right)/2)];
     }
 
     lower() {
-    	this.max = Math.floor((this.max + this.min)/2)+1;
+        this.right = Math.floor((this.left+this.right)/2) + 1;
     }
 
     greater() {
-    	this.min = Math.floor((this.max + this.min)/2);
+        this.left = Math.floor((this.left+this.right)/2) ;
     }
 }
 
